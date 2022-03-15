34902171a6bd9a92ec60fe2d6cbdae80153b6fa2
Kai Pak <kaipak@jpl.nasa.gov>
2022-03-15 09:55:37 -0700

    Testing for mslv2_dataset generation.

--- src/datasets.py
+++ src/datasets.py
@@ -9,7 +9,12 @@ class CloverDatasets(object):
 
     def __init__(self, data_path: str = 'clover_shared/datasets',
                  out_path: str = 'datasets/clover_datasets',
-                 msl_class_map: str = 'msl_synset_words-indexed.txt'):
+                 msl_class_map: str = None):
+        """Create datasets for use in Pytorch based SSL Models from CLOVER datasets
+
+        """
+        print(f"Setting up CloverDatasets with {data_path} as input source and "
+              f"{out_path} for output.\n")
         self.data_path = Path(data_path)
         self.out_path = Path(out_path)
         self.msl_class_map = msl_class_map
@@ -21,17 +26,18 @@ class CloverDatasets(object):
         train_path = self.out_path / msl_dataset_dir / 'train'
         self.df_msl_train = pd.read_csv(train_file, sep='\s', names=['img', 'label'])
 
-        print(f"Deleting existing dataset at {self.out_path / msl_dataset_dir}")
+        print(f"Deleting existing dataset at {self.out_path / msl_dataset_dir}\n")
         if train_path.exists() and train_path.is_dir():
             shutil.rmtree(train_path)
         train_path.mkdir(parents=True, exist_ok=True)
 
-        for i in range(25):
-            print(train_path / f'class_{i}')
+        for i in self.df_msl_train.label.unique():
             (train_path / f'class_{i}').mkdir()
         for i, row in self.df_msl_train.iterrows():
             shutil.copy(self.data_path / f'msl-labeled-data-set-v2.1/images/{row.img}',
                         self.out_path / train_path / f'class_{row.label}')
+        value_counts = self.df_msl_train['label'].value_counts()
+        print(f"MSLv2 training dataset summary:\n {value_counts}")
 
 
 
